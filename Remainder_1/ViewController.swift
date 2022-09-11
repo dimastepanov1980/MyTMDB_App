bd33b2bcfe2ab312b144c0d152975edbd4096547
Dima <dimastepanov1980@gmail.com>
2022-09-11 09:06:29 +0700

    Initial Commit

---
+++ Remainder_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Remainder_1
+//
+//  Created by Dmitriy Stepanov on 11.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
